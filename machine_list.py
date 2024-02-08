4390f5ad3c907037d7017c16639349fd778ae527
vagrant <vagrant@sloopstash-ubuntu-22-04-server>
2024-02-08 07:12:14 +0000

    Add template labs

--- machine_list.py
+++ machine_list.py
@@ -1,21 +1,21 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import json
 
 def get_inventory():
-    df = pd.read_excel('inventory.xlsx')
+    df = pd.read_excel('/home/vagrant/inventory.xlsx')
 
     inventory = {}
     
     for index, row in df.iterrows():
         group = row['group']
         host = row['host']
-        ip = row['ip']
 
         if group not in inventory:
-            inventory[group] = {'hosts': [], 'ips':[]}
+            inventory[group] = {'hosts': []}
         
         inventory[group]['hosts'].append(host)
-        inventory[group]['ips'].append(ip)
 
     print(json.dumps(inventory))
 
